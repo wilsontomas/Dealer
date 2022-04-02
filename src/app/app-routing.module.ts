c4ee46eeed592e3b836ff02c53b1a43e2ddab3d5
Wilson Tomas <wilsonpumm@gmail.com>
2022-04-02 12:05:03 -0300

    se agregaron los estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+  },
+
+  {
+    path:'shop',
+    component:ShopComponent,
+  },
+  {
+    path:'detail/:id',
+    component:DetailComponent,
+  },
+  {
+    path:'contact',
+    component:ContactComponent,
+  },
+  {
+    path:'about',
+    component:AboutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
